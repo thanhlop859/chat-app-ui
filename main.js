007ce7f3761e12c763a25fe3df45e95132d2b93a
Lê Duy Thanh <thanhlop859@gmail.com>
2020-11-12 20:38:05 +0700

    create list chatting and modified box-chat

--- main.js
+++ main.js
@@ -74,18 +74,68 @@ var userInfor =
         ],
         acceptFriendRequest:[]
 };
+var listChatting=[
+    {
+        chatId:"002",
+        titleChat:"Le duy thanh", // ten người chat hoặc nhóm đang chat
+        message:[
+            {
+                email:"kazz@gmail.com", //đại diện người gửi
+                nameSender:"Đình Vũ",
+                status:'1',//status : trang thai của tin nhắn 0 là gửi 1 là nhận
+                content:"Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittisdictum nisi"
+            },
+            {
+                nameSender:"Đình Vũ",
+                email:"kazz@gmail.com", //đại diện người gửi
+                status:'1',
+                content:'Tin nhan tu vu'
+            },
+            {   
+                nameSender:"Đình Vũ",
+                email:"kazz@gmail.com", //đại diện người gửi
+                status:'0',
+                content:'tinh nhan tu dinh vu'
+            }
+        ]
+    },
+    {
+        chatId:'001',
+        email:"ak@gmail.com",
+        message:[
+            {
+                email:"kazz@gmail.com", //đại diện người gửi
+                nameSender:"Đình Vũ",
+                status:'1',//status : trang thai của tin nhắn 0 là gửi 1 là nhận
+                content:'a'
+            },
+            {
+                nameSender:"Đình Vũ",
+                email:"kazz@gmail.com", //đại diện người gửi
+                status:'1',
+                content:'a'
+            },
+            {   
+                nameSender:"Đình Vũ",
+                email:"kazz@gmail.com", //đại diện người gửi
+                status:'0',
+                content:'a'
+            }
+        ]
+    }
+];
 // 3 request 
 // - lay thong tin user
 // - lay danh sách bạn 
 // - lay danh sách request 
 // - danh sách 
 
-
+/**********************************************************************/
 // đợi tài liệu load và hiển thị danh sách bạn 
 function scriptDisplay(){
     //displayListFriend();
 }
-
+// --------------------------------------------------------------------
 //hàm hiển thị danh sách bạn
 function displayListFriend(){
     var selectHtml = document.querySelector(".listFriend");
@@ -100,50 +150,6 @@ function displayListFriend(){
     offPlayout('btnAddGroup','display_none');
 }
 // -----------------------------------------------------------
-
-// đăt lại trạng thái fucus
-function friendOnClick(nodeFriend){
-    var friendElements = document.getElementsByClassName('friend');
-    for(var i= 0;i< friendElements.length;i++){
-        friendElements[i].setAttribute("class",'friend');
-    }
-    nodeFriend.setAttribute("class","friend active");
-
-    // hàm hiển thị khung chat
-    displayFrameChat(nodeFriend);
-    
-}
-//-----------------------------------------------------------
-
-// hiển thị khung chat 
-function displayFrameChat(node){
-    var id =node.getAttribute("id");
-    //lấy thông tin của bạn
-    //var inforFriend =  httpGet(url+"users/"+id);
-
-    var inforFriend ={
-        email:"hai@gmail.com",          //dữu liệu demo
-            username: "Lê Đinh Vũ"
-    }
-    // hiển thị tên người đang chat 
-    document.getElementsByClassName("titleBox")[0].innerText= inforFriend.username;
-    
-    // hiển thị khung chat cho người được chọn
-    //
-    //
-    //
-
-    // set input message gửi đến ai 
-    //
-    //
-    //
-    document.getElementsByClassName("chatBox")[0].setAttribute("class","chatBox display");
-
-    //tắt nút thêm nhóm
-    offPlayout('btnAddGroup','display_none');
-}
-//----------------------------------------------------------------------------------
-
 // hiển thị danh sách nhóm đã join
 function displayListGroup(){
     document.getElementsByClassName("listFriend")[0].style.display="grid";
@@ -169,7 +175,72 @@ function displayListRequest(){
     selectHtml.innerHTML =s.join('');   
     offPlayout('btnAddGroup','display_none');
 }
+// hàm hiển thị thông tin cá nhân và đăng xuất
+function displayProfile(){
+    document.getElementsByClassName("listFriend")[0].style.display="none";
+    var a = document.getElementsByClassName("myProfile")[0];
+    a.style.display ="flex";
+    document.getElementById('myName').innerText = "Tên : "+userInfor.username;
+    document.getElementById('myAge').innerText = "Tuổi : "+userInfor.age;
+    document.getElementById("myGender").innerText = "Giới tính : "+userInfor.gender;
+    document.getElementById('myEmail').innerText = "Địa chỉ Email : "+userInfor.email;
+   
+    offPlayout('btnAddGroup','display_none');// tắt nút thêm nhóm
 
+}
+
+/****************************************************************** */
+
+// đăt lại trạng thái fucus
+function friendOnClick(nodeFriend){
+    var friendElements = document.getElementsByClassName('friend');
+    for(var i= 0;i< friendElements.length;i++){
+        friendElements[i].setAttribute("class",'friend');
+    }
+    nodeFriend.setAttribute("class","friend active");
+
+    // hàm hiển thị khung chat
+    displayFrameChat(nodeFriend);
+    
+}
+/*****************************************************************/
+// hiển thị khung chat 
+function displayFrameChat(node){
+    var id =node.getAttribute("id");
+    //lấy thông tin của bạn
+    //var inforFriend =  httpGet(url+"users/"+id);
+
+    var inforFriend ={
+        email:"hai@gmail.com",          //dữu liệu demo
+            username: "Lê Đinh Vũ"
+    }
+    // hiển thị tên người đang chat 
+    document.getElementsByClassName("titleBox")[0].innerText= listChatting[0].titleChat;
+    var a = document.getElementById('ariaMessage');
+    listChatting[1].message.forEach(x=>a.innerHTML += `<div class="stl_mes send"><div></div><span>`+ x.content+`</span></div>`);
+    //tự động cuộn xuống nội dung mới 
+    var a = document.getElementById('ariaMessage');
+    a.scrollTop = a.scrollHeight;
+
+
+
+    // hiển thị khung chat cho người được chọn
+    //
+    //
+    //
+
+    // set input message gửi đến ai 
+    //
+    //
+    //
+    document.getElementsByClassName("chatBox")[0].setAttribute("class","chatBox display");
+
+    //tắt nút thêm nhóm
+    offPlayout('btnAddGroup','display_none');
+}
+
+
+/*******************************************************************/
 
 // hàm đóng khung chat lại
 function offDislayChat(){
@@ -190,19 +261,7 @@ function onDisplayInfor(){
 }
 
 
-// hàm hiển thị thông tin cá nhân và đăng xuất
-function displayProfile(){
-    document.getElementsByClassName("listFriend")[0].style.display="none";
-    var a = document.getElementsByClassName("myProfile")[0];
-    a.style.display ="flex";
-    document.getElementById('myName').innerText = "Tên : "+userInfor.username;
-    document.getElementById('myAge').innerText = "Tuổi : "+userInfor.age;
-    document.getElementById("myGender").innerText = "Giới tính : "+userInfor.gender;
-    document.getElementById('myEmail').innerText = "Địa chỉ Email : "+userInfor.email;
-   
-    offPlayout('btnAddGroup','display_none');// tắt nút thêm nhóm
 
-}
 
 //Hiển thị form cập nhật
 function onDisplayFormUpdate(){
@@ -233,5 +292,5 @@ function onDisplayPlayout(idPlayout,classDel){
 function offPlayout(idPlayout,classInsert){
     var a = document.getElementById(idPlayout).getAttribute("class"); 
     if(a.search(classInsert)==(-1))
-    document.getElementById(idPlayout).setAttribute("class"," "+a+classInsert);  
+    document.getElementById(idPlayout).setAttribute("class",a+classInsert); 
 }
